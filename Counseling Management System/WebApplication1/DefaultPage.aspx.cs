cd2dad93a29dc1b8c0241fff27180bb551442528
Ma2Ri2aL <95554293+Ma2Ri2aL@users.noreply.github.com>
2023-05-18 01:48:28 +0800

    This progress still have issue
    This progress still have issue

---
+++ Counseling Management System/WebApplication1/DefaultPage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class DefaultPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            USER u = new USER();
+            u.addUser(txtAddfName.Text, txtAddlName.Text, txtAddPosition.Text, txtAddRoles.Text, txtAddpNumber.Text, 
+                        txtAddEMail.Text, txtAddAddress.Text, int.Parse(txtAddAge.Text));
+        }
+    }
+}
